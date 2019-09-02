1c3b5d0e45db90692c0014d9e15392ac4b1d2f70
zlds <h1091236035@gmail.com>
2019-09-02 13:59:31 +0800

    通过admin管理内容

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,3 +1,67 @@
 from django.contrib import admin
+from django.shortcuts import reverse
+from django.utils.html import format_html
 
+import sys
+sys.path.append("..")
+
+from .models import Category, Tag, Post
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
+    fields = ('name', 'status', 'is_nav', 'owner')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+    post_count.short_description = "文章数量"
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(TagAdmin, self).save_model(request, obj, form, change)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'category', 'status',
+        'created_time', 'operator'
+    ]
+    # list_display_links = []
+    #配置那些通过
+    search_fields = ['title', 'category__name']
+
+    # actions_on_top = True
+    actions_on_bottom = False
+    #编辑页面
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+
+    def operator(self, obj):
+        return format_html(
+            '<a herf="{}">编辑</a>',
+            reverse('admin:blog_post_change', args=(obj.id,))
+        )
+    operator.short_description = '操作'
+
+    def save_model(self, request, obj, form, change):
+        print(request)
+        print(obj)
+        print(form)
+        print(change)
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -11,15 +11,18 @@ class Category(models.Model):
         (STATUS_DELETE, "删除"),
     )
 
-    name = models.CharField(max_length=50,verbose_name="名称")
+    name = models.CharField(max_length=50, verbose_name="名称")
     status = models.PositiveIntegerField(default=STATUS_NORMAL,
                                          choices = STATUS_ITEMS, verbose_name="状态")
     is_nav = models.BooleanField(default=False, verbose_name="是否为导航")
     owner = models.ForeignKey(User, verbose_name="作者")
-    created_time = models.DateTimeField(auto_now_add=True,verbose_name="创建时间")
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
         verbose_name = verbose_name_plural = '分类'
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
@@ -38,6 +41,9 @@ class Tag(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = "标签"
 
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
@@ -48,16 +54,19 @@ class Post(models.Model):
         (STATUS_DRAFT,"草稿")
     )
 
-    title = models.CharField(max_length=255,verbose_name="标题")
-    desc = models.CharField(max_length=1024,blank=True,verbose_name="摘要")
-    content = models.TextField(verbose_name="正文",help_text="正文必须为MarkDown")
+    title = models.CharField(max_length=255, verbose_name="标题")
+    desc = models.CharField(max_length=1024,blank=True, verbose_name="摘要")
+    content = models.TextField(verbose_name="正文", help_text="正文必须为MarkDown")
     status = models.PositiveIntegerField(default=STATUS_NORMAL,
         choices=STATUS_ITEMS, verbose_name="状态")
     category = models.ForeignKey(Category, verbose_name="分类")
     tag = models.ManyToManyField(Tag, verbose_name="标签")
     owner = models.ForeignKey(User, verbose_name="作者")
-    created_time = models.DateTimeField(auto_now_add=True,verbose_name="创建时间")
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
         verbose_name = verbose_name_plural = "文章"
         ordering = ['-id']
+
+    def __str__(self):
+        return self.title
